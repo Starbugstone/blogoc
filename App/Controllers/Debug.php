3113dea17ff00b1d75e76c549404c10dd7e9a29d
StarbugStone <pint2stone@gmail.com>
2018-08-06 23:52:00 +0200

    Usiing traits works
    This method works but feals a bit clunky.

--- App/Controllers/Debug.php
+++ App/Controllers/Debug.php
@@ -1,7 +1,10 @@
 <?php
 namespace App\Controllers;
+use \Core\View;
+
 
 class Debug{
+    use \App\Traits\Navigation;
     public function index($test = ''){
         echo 'Ok '.$test.'<br>';
         echo 'the router is working \\o/';
@@ -14,5 +17,11 @@ class Debug{
     public function testNav(){
         $nav = new \App\Controllers\Template\Navigation();
         echo $nav->index();
+    }
+
+    public function testNav2(){
+
+        $data = \App\Traits\Navigation::getMenu();
+        echo View::returnTemplate('Template/Navigation.twig', $data);
     }
 }
---
+++ App/Traits/Navigation.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Traits;
+
+trait Navigation{
+    public static function getMenu(){
+        //this shall be replaced by a model call
+        $data['navigation'] = [
+            'home' => '/',
+            'category1' => '/cat/1',
+            'category2' => '/cat/2',
+            'category3' => '/cat/3'
+        ];
+
+        return $data;
+
+    }
+}
