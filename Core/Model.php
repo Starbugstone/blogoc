457db0b56ee489dc4f7e7b0e22c03cbf450368f2
StarbugStone <pint2stone@gmail.com>
2018-08-09 14:09:55 +0200

    code cleanup
    added some doc and code dleanup

--- Core/Model.php
+++ Core/Model.php
@@ -1,13 +1,36 @@
 <?php
+
 namespace Core;
+
 use App\Config;
 use PDO;
 
-abstract class Model{
+/**
+ * Class Model here we have all the generic calls to be inherited by the App\Models
+ * @package Core
+ */
+abstract class Model
+{
+    /**
+     * @var string $host stores the DataBase Host grabbed from the config
+     */
     private $host = Config::DB_HOST;
-    private $db   = Config::DB_NAME;
+    /**
+     * @var string $db stores the DataBase Name grabbed from the config
+     */
+    private $db = Config::DB_NAME;
+    /**
+     * @var string $user stores the DataBase User Name grabbed from the config
+     */
     private $user = Config::DB_USER;
+    /**
+     * @var string $pass stores the DataBase User password grabbed from the config
+     */
     private $pass = Config::DB_PASSWORD;
+
+    /**
+     * @var string $charset forcing the charset to UTF8
+     */
     private $charset = 'utf8';
 
     protected $dbh; //database handler
@@ -20,7 +43,7 @@ abstract class Model{
     {
         $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset"; //Creating the Data Source name
         $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
         $this->dbh = new PDO($dsn, $this->user, $this->pass, $opt);
