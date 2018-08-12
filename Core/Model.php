c8e01e70c69ea415a6223934b19f52c3a4309f80
StarbugStone <pint2stone@gmail.com>
2018-08-12 13:31:23 +0200

    finished the generic get table name ad adding some generic querys
    The get table name function is finished.
    If no table name is passed, we check for a table with the model name.
    If none exist, we check for a view with v_modelName.
    
    We can also pass in a table or view name. The program will still do the check.
    
    Also added a few generic selects :
    getresultSet -> gets the entire table
    getRowById -> pass in an ID and it returns the line
    getRowByColumn -> pass in a column name and a value and it returns the coresponding line.
    
    The last function probably needs some sanitizing to avoid SQL injections on the columnname as PDO doesn't alow prepared statments with column or table names
    
    Also need to docu the functions

--- Core/Model.php
+++ Core/Model.php
@@ -46,13 +46,13 @@ abstract class Model
      */
     public function __construct()
     {
-            $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset"; //Creating the Data Source name
-            $opt = [
-                PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ];
-            $this->dbh = new PDO($dsn, $this->user, $this->pass, $opt);
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset"; //Creating the Data Source name
+        $opt = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+        $this->dbh = new PDO($dsn, $this->user, $this->pass, $opt);
     }
 
     /*
@@ -64,8 +64,8 @@ abstract class Model
      * creating and storing the query
      * @param $sql string creating the sql query
      */
-    protected function query($sql):void{
-
+    protected function query($sql): void
+    {
         $this->stmt = $this->dbh->prepare($sql);
     }
 
@@ -75,10 +75,11 @@ abstract class Model
      * @param null $type
      * @throws \Exception error if no sql query to bind to
      */
-    protected function bind($param, $value, $type = null):void{
-        if($this->stmt != null){
-            if (is_null($type)){ //need a bind value, so just check it in code. that way we can just call bind(param,value)
-                switch(true){
+    protected function bind($param, $value, $type = null): void
+    {
+        if ($this->stmt != null) {
+            if (is_null($type)) { //need a bind value, so just check it in code. that way we can just call bind(param,value)
+                switch (true) {
                     case is_int($value):
                         $type = PDO::PARAM_INT;
                         break;
@@ -93,16 +94,17 @@ abstract class Model
                 }
             }
             $this->stmt->bindValue($param, $value, $type);
-        }else{
+        } else {
             throw new \Exception("No query to bind to");
         }
 
     }
 
-    protected function execute(){
-        if($this->stmt != null){
+    protected function execute()
+    {
+        if ($this->stmt != null) {
             return $this->stmt->execute();
-        }else{
+        } else {
             throw new \Exception("No statement to execute");
         }
 
@@ -115,38 +117,93 @@ abstract class Model
 
     /**
      * correlation between the model name and the table name
-     *if we don't have a table name, get the table that has the same name as the model will be returned (else, we do nothing !!)
+     * if we don't have a table name, get the table that has the same name as the model will be returned (else, we do nothing !!)
      * Also search if the table exists, if not do a check in the views (must be v_$table)
      * @param string|null $table the name of the table to get, if none the get the table of the models name
      * @return string the table name (with an s)
      * @throws \ReflectionException the model doesn't exist, should never happen
      * @throws \Exception table or view doesn't exist
+     * @return string table or view name
      */
-    private function getTable(string $table=null){
-        if($table == null){
+    private function getTable(string $table = null): string
+    {
+        //If no table is passed, get the calling model name
+        if ($table == null) {
             $reflect = new \ReflectionClass(get_class($this));
             $table = $reflect->getShortName(); //this is to only get the model name, otherwise we get the full namespace
-            $table = $table.'s';
+            $table = $table . 's'; //adding the s since the table should be plural. Might be some special case where the plural isn't just with an s
+            $table = strtolower($table); //the database names are in lowercase
         }
 
-        //IF table exists
-            //SQL search for tables : show full tables where Table_Type = 'BASE TABLE'
-            //or show full tables where Table_Type != 'VIEW'
-            //$result = mysql_query("SHOW TABLES LIKE 'myTable'");
-            //$tableExists = mysql_num_rows($result) > 0;
-            //TODO Testing with Db needed
-            //--------------
-            //return table
-        //ELSEIF v_table exists
-            //return view
-        //ELSE
-            //throw error table or view doesn't exist
-        return $table;
+        //see if table exists
+        $sql = "SHOW TABLES LIKE :table";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->bindValue(':table', $table, PDO::PARAM_STR);
+        $stmt->execute();
+        $exists = $stmt->rowCount() > 0; //will return 1 if table exists or 0 if non existant
+
+        if ($exists) {
+            //the table exists
+            return $table;
+        }
+
+        //if we are here, then table doesn't exist, check for view
+        $view = 'v_' . $table;
+        $stmt->bindValue(':table', $view, PDO::PARAM_STR);
+        $stmt->execute();
+        $exists = $stmt->rowCount() > 0; //will return 1 if table exists or 0 if non existant
+
+        if ($exists) {
+            //the view exists
+            return $view;
+        }
+
+        //neither table or view exists
+        //throw an error
+        throw new \Exception("Table or view $table doesn't exist");
     }
 
-    protected function test(){
-        echo '<p>'.get_class($this).' <- Our Class</p>';
+    /**
+     * gets the entire table or view and returns as an object
+     * @param string $table
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    protected function getResultSet($table = ''){
+        $tableName = $this->getTable($table);
+        $sql = "SELECT * FROM $tableName"; //can not pass table name as :parameter. since we already have tested if the table exists, this var should be safe.
+        $this->query($sql);
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
 
-        echo '<p>'.$this->getTable().' <- Our shortend Class</p>';
+    protected function getRowById($id, $table=''){
+        $tableName = $this->getTable($table);
+        $idName = 'id'.$tableName;
+        $sql = "SELECT * FROM $tableName WHERE $idName = :id";
+        $this->query($sql);
+        $this->bind(':id',$id);
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    protected function getRowByColumn($columnName,$value,$table=''){
+        $tableName = $this->getTable($table);
+        //TODO sanitize $columnName
+        $sql = "SELECT * FROM $tableName WHERE $columnName = :value";
+        $this->query($sql);
+        $this->bind(':value', $value);
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+
+    protected function test()
+    {
+        //echo '<p>' . get_class($this) . ' <- Our Class</p>';
+
+        echo '<p>' . $this->getTable() . ' <- the table or view</p>';
+        $resu = $this->getRowById('1','v_debug');
+        var_dump($resu);
     }
 }
