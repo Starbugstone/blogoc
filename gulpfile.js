b19a7f22b98c995960e4f65cad9d156e6aaa35a5
StarbugStone <pint2stone@gmail.com>
2018-08-13 15:58:14 +0200

    reding the layout for a cleaner style
    Redoing the menu and jumbo.
    Added the carousel to the jumbo text + animated
    Also incorporated some scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp'),
-	browserSync = require('browser-sync').create(),
 	sass = require('gulp-sass');
 	
-gulp.task('sass', function () {
-  return gulp.src('./public/sass/**/*.scss')
+gulp.task('scss', function () {
+  return gulp.src('./public/scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./public/sass/**/*.scss', ['sass']);
+gulp.task('scss:watch', function () {
+  gulp.watch('./public/scss/**/*.scss', gulp.series('scss'));
 });
