7d36ae7f23995f51a48ffab0258ff77b00197801
StarbugStone <pint2stone@gmail.com>
2018-08-13 22:48:57 +0200

    correctiong codacity errors
    Ok. corrected.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass');
+var gulp = require("gulp"),
+	sass = require("gulp-sass");
 	
-gulp.task('scss', function () {
-  return gulp.src('./public/scss/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+gulp.task("scss", function () {
+  return gulp.src("./public/scss/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(gulp.dest("./public/css"));
 });
  
-gulp.task('scss:watch', function () {
-  gulp.watch('./public/scss/**/*.scss', gulp.series('scss'));
+gulp.task("scss:watch", function () {
+  gulp.watch("./public/scss/**/*.scss", gulp.series("scss"));
 });
--- public/js/script.js
+++ public/js/script.js
@@ -7,8 +7,8 @@ var MQL = 992; //Minimum screen width, don't want the effect on mobile
 
 //primary navigation slide-in effect
 if ($(window).width() > MQL) {
-    var headerHeight = $('#mainNav').height();
-    $(window).on('scroll', {
+    var headerHeight = $("#mainNav").height();
+    $(window).on("scroll", {
             previousTop: 0
         },
         function() {
@@ -16,19 +16,20 @@ if ($(window).width() > MQL) {
             //check if user is scrolling up
             if (currentTop < this.previousTop) {
                 //if scrolling up...
-                if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
-                    $('#mainNav').addClass('is-visible');
+                if (currentTop > 0 && $("#mainNav").hasClass('is-fixed')) {
+                    $("#mainNav").addClass('is-visible');
                 } else {
-                    $('#mainNav').removeClass('is-visible is-fixed');
+                    $("#mainNav").removeClass('is-visible is-fixed');
                 }
             } else if (currentTop > this.previousTop) {
                 //if scrolling down...
-                $('#mainNav').removeClass('is-visible');
-                if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+                $("#mainNav").removeClass('is-visible');
+                if (currentTop > headerHeight && !$("#mainNav").hasClass('is-fixed')) $("#mainNav").addClass('is-fixed');
             }
             this.previousTop = currentTop;
         });
 }
+
 //---------------------------------------------
 //end navbar
 //---------------------------------------------
@@ -37,4 +38,5 @@ if ($(window).width() > MQL) {
 //---------------------------------------------
 // Init wow for animation on scroll
 //---------------------------------------------
+/*global WOW*/
 new WOW().init();
