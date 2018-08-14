152b1d79a6171c3639aff48d8ab460b2168e18b0
StarbugStone <pint2stone@gmail.com>
2018-08-14 10:18:25 +0200

    Final cleanup
    Codacity is happy, Just a few comment and var name modifications for easier reading.

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
 //---------------------------------------------
 // The navbar on top if scroll up
 //---------------------------------------------
-//navbar shown on scroll
+//navbar slide in effects and shown on scroll
 //---------------------------------------------
 
-function scrollingUp($currentTop){
+function isScrollingUp($currentTop) {
     if ($currentTop > 1 && $("#mainNav").hasClass("is-fixed")) {
         $("#mainNav").addClass("is-visible");
     } else {
@@ -12,34 +12,31 @@ function scrollingUp($currentTop){
     }
 }
 
-function scrollingDown($currentTop){
+function isScrollingDown($currentTop) {
     $("#mainNav").removeClass("is-visible");
-    if ($currentTop > $("#mainNav").height() && !$("#mainNav").hasClass("is-fixed")){
+    if ($currentTop > $("#mainNav").height() && !$("#mainNav").hasClass("is-fixed")) {
         $("#mainNav").addClass("is-fixed");
     }
 }
 
 //setting the initial position to 0
 var $previousTop = 0;
-// Show the navbar when the page is scrolled up
-var MQL = 992; //Minimum screen width, don't want the effect on mobile. Avoids IOS bug
 
-//primary navigation slide-in effect
+//Minimum screen width, don't want the effect on mobile. Avoids IOS stuttering bug
+var MQL = 992;
+
 if ($(window).width() > MQL) {
     $(window).on("scroll",
-        function() {
+        function () {
             var $currentTop = $(window).scrollTop(); //our position in the window, returns the number of pixels from the window top to the site top
-            //check if user is scrolling up
+            //check if user is scrolling up or down
             if ($currentTop < $previousTop) {
-                //if scrolling up...
-                scrollingUp($currentTop);
+                isScrollingUp($currentTop);
             } else {
-                //scrolling down...
-                scrollingDown($currentTop);
+                isScrollingDown($currentTop);
             }
             $previousTop = $currentTop;
-        }
-        );
+        });
 }
 
 //---------------------------------------------
