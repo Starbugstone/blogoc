3a164c1eccd2f5a13980c27d24c7452ce47d0942
StarbugStone <pint2stone@gmail.com>
2018-08-20 00:55:12 +0200

    Using the same mthod as Ajax
    This works better, but I will have a problem with the ajax needing some admin auth.

--- Core/AdminController.php
+++ Core/AdminController.php
@@ -1,6 +1,23 @@
 <?php
 namespace Core;
 
-class AdminController extends Controller{
-    
+abstract class AdminController extends Controller{
+    public function __construct(Container $container)
+    {
+        parent::__construct($container);
+
+    }
+
+    public function getUserLevel(){
+        //get session level from the actual $_SESSION
+        //we could use a binary system for the rights but not much granular levels to take care of
+        $sessionLevel = 0;
+        if($sessionLevel === 1){
+            return 'User';
+        }
+        if($sessionLevel === 2){
+            return 'Admin';
+        }
+        return 'Visitor'; //we should do a redirect to home with message popup. Need to implement popups
+    }
 }
