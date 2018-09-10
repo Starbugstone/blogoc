c67e399df144094d1aef2177c113ffefbd82332b
StarbugStone <pint2stone@gmail.com>
2018-09-10 13:13:06 +0200

    Added extra table for config type
    Exported the config type to seperate table
    updated the model
    added HTML5 input type

--- DataBase/TableStructure.sql
+++ DataBase/TableStructure.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `blogoc` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `blogoc`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: blogoc
@@ -98,7 +96,7 @@ CREATE TABLE `configs` (
 
 LOCK TABLES `configs` WRITE;
 /*!40000 ALTER TABLE `configs` DISABLE KEYS */;
-INSERT INTO `configs` VALUES (1,'front_text_1','Developping tomorrows Web','string',1),(2,'front_text_2','If I can imagine it','string',1),(3,'front_text_3','Smoke me a kipper,','string',1),(4,'social_icons_linkedin','https://www.linkedin.com/in/matthew-clancy-024597ba/','url',2),(5,'social_icons_github','https://github.com/Starbugstone','url',2),(6,'social_icons_twitter','https://twitter.com/StarbugStone','url',2),(7,'social_icons_facebook','','url',2),(8,'social_icons_website','https://starbugstone.eu','url',2),(9,'site_name','Blog OC','string',4),(10,'about_me_image','https://pbs.twimg.com/profile_images/3676317209/243cf055afcb2baa9fd894e5305985fa_400x400.jpeg','image',3),(11,'front_text_1_subtext','The starbug way','string',1),(12,'front_text_2_subtext','I can probably code it !','string',1),(13,'front_text_3_subtext','I\'ll be back for breakfast','string',1);
+INSERT INTO `configs` VALUES (1,'front_text_1','Developping tomorrows Web','1',1),(2,'front_text_2','If I can imagine it','1',1),(3,'front_text_3','Smoke me a kipper,','1',1),(4,'social_icons_linkedin','https://www.linkedin.com/in/matthew-clancy-024597ba/','2',2),(5,'social_icons_github','https://github.com/Starbugstone','2',2),(6,'social_icons_twitter','https://twitter.com/StarbugStone','2',2),(7,'social_icons_facebook','','2',2),(8,'social_icons_website','https://starbugstone.eu','2',2),(9,'site_name','Blog OC','1',4),(10,'about_me_image','https://pbs.twimg.com/profile_images/3676317209/243cf055afcb2baa9fd894e5305985fa_400x400.jpeg','3',3),(11,'front_text_1_subtext','The starbug way','1',1),(12,'front_text_2_subtext','I can probably code it !','1',1),(13,'front_text_3_subtext','I\'ll be back for breakfast','1',1);
 /*!40000 ALTER TABLE `configs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,6 +126,30 @@ INSERT INTO `configs_class` VALUES (1,'20_front_page_text'),(2,'21_front_page_so
 UNLOCK TABLES;
 
 --
+-- Table structure for table `configs_type`
+--
+
+DROP TABLE IF EXISTS `configs_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `configs_type` (
+  `idconfigs_type` int(11) NOT NULL AUTO_INCREMENT,
+  `configs_type_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`idconfigs_type`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `configs_type`
+--
+
+LOCK TABLES `configs_type` WRITE;
+/*!40000 ALTER TABLE `configs_type` DISABLE KEYS */;
+INSERT INTO `configs_type` VALUES (1,'text'),(2,'url'),(3,'image');
+/*!40000 ALTER TABLE `configs_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `posts`
 --
 
@@ -152,7 +174,7 @@ CREATE TABLE `posts` (
   KEY `fk_post_category1_idx` (`categories_idcategories`),
   CONSTRAINT `fk_post_category1` FOREIGN KEY (`categories_idcategories`) REFERENCES `categories` (`idcategories`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_post_user1` FOREIGN KEY (`author_iduser`) REFERENCES `users` (`idusers`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -161,7 +183,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,'TEST POST',NULL,3,'BLA',1,'0000-00-00 00:00:00','0000-00-00 00:00:00',1,1,'bla');
+INSERT INTO `posts` VALUES (1,'TEST POST','http://lorempixel.com/400/200/',3,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum, erat id congue imperdiet, dolor magna porta ex, id faucibus justo nulla vel erat. Ut auctor, ligula et luctus vestibulum, purus mauris vulputate leo, id imperdiet felis odio eget augue. Aliquam ut sagittis sem. Nunc ut mi porttitor, aliquam leo nec, dignissim dui. Ut at dolor nisl. Cras congue at quam eu dapibus. Nullam id est vitae nunc volutpat auctor sit amet eget turpis.',1,'0000-00-00 00:00:00','0000-00-00 00:00:00',1,1,'bla'),(2,'TEST POST2','http://lorempixel.com/400/200/',3,'dolor sit amet, consectetur adipiscing elit. Nam condimentum, erat id congue imperdiet, dolor magna porta ex, id faucibus justo nulla vel erat. Ut auctor, ligula et luctus vestibulum, purus mauris vulputate leo, id imperdiet felis odio eget augue. Aliquam ut sagittis sem. Nunc ut mi porttitor, aliquam leo nec, dignissim dui. Ut at dolor nisl. Cras congue at quam eu dapibus. Nullam id est vitae nunc volutpat auctor sit amet eget turpis. Aenean eget lectus vel ante malesuada pellentesque quis aliquam justo. Nulla a vehicula libero. Curabitur nec accumsan leo. Nunc sagittis velit nec mollis luctus. Nam in suscipit velit, ut imperdiet dolor. Nullam nec elit congue, semper nisi quis, commodo magna. Vivamus sagittis, urna vel fringilla scelerisque, metus nulla mattis lorem, eget viverra justo ligula sit amet ex. In tempor consequat tortor. Morbi ullamcorper eros at velit accumsan, blandit tincidunt elit viverra. Aenean tincidunt mattis tortor. Phasellus id nisi ornare, fermentum nisi in, egestas dolor. Pellentesque purus nulla, congue sit amet libero dignissim, varius facilisis metus. Sed sed lacinia nunc. Aliquam quis gravida est. Morbi ut aliquam diam.',1,'0000-00-00 00:00:00','0000-00-00 00:00:00',1,1,'bla2'),(3,'TEST POST3','http://lorempixel.com/400/200/',3,'BLA Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum, erat id congue imperdiet, dolor magna porta ex, id faucibus justo nulla vel erat. Ut auctor, ligula et luctus vestibulum, purus mauris vulputate leo, id imperdiet felis odio eget augue. Aliquam ut sagittis sem. Nunc ut mi porttitor, aliquam leo nec, dignissim dui. Ut at dolor nisl. Cras congue at quam eu dapibus. Nullam id est vitae nunc volutpat auctor sit amet eget turpis.',1,'0000-00-00 00:00:00','0000-00-00 00:00:00',1,1,'bla3'),(4,'TEST POST4','http://lorempixel.com/400/200/',3,'BLA',1,'0000-00-00 00:00:00','0000-00-00 00:00:00',1,1,'bla4');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -295,4 +317,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-24 15:31:33
+-- Dump completed on 2018-09-10 13:12:04
