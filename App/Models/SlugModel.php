702ea9bb5570f07319315d0d33f448092199f4a9
StarbugStone <pint2stone@gmail.com>
2018-09-15 13:42:03 +0200

    security check before checking SLUG
    Making sure we have only alphanum and _ or - as table and column name for security.

--- App/Models/SlugModel.php
+++ App/Models/SlugModel.php
@@ -10,9 +10,16 @@ class SlugModel extends Model
 
     public function isUnique(string $slug, string $table, string $columnName)
     {
-        $slugTbl = $this->getTablePrefix($table);
 
-        //TODO verify vars
+        if (!preg_match("/^[A-Za-z0-9_-]+$/", $table)) {
+            throw new \ErrorException("Invalid table name " . $table);
+        }
+
+        if (!preg_match("/^[A-Za-z0-9_-]+$/", $columnName)) {
+            throw new \ErrorException("Invalid Column name " . $columnName);
+        }
+
+        $slugTbl = $this->getTablePrefix($table);
 
         $sql = "SELECT * FROM $slugTbl WHERE $columnName = :slug";
         $stmt = $this->dbh->prepare($sql);
