4cf4c4618ddc665de505eeaf1abff70302290ad5
StarbugStone <pint2stone@gmail.com>
2018-10-08 16:59:54 +0200

    FrontEnd template for login with and without JS

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -10,6 +10,61 @@ use Core\Controller;
 class Login extends Controller
 {
 
+    protected $siteConfig;
+
+    public function __construct(Container $container)
+    {
+        $this->loadModules[] = 'SiteConfig';
+        parent::__construct($container);
+    }
+
+
+    /**
+     * the login form
+     */
+    public function index()
+    {
+        $this->sendSessionVars();
+        $this->data['configs'] = $this->siteConfig->getSiteConfig();
+        $this->data['navigation'] = $this->siteConfig->getMenu();
+
+        $this->renderView('logon');
+    }
+
+    /**
+     * the register form
+     */
+    public function register()
+    {
+        $this->sendSessionVars();
+        $this->data['configs'] = $this->siteConfig->getSiteConfig();
+        $this->data['navigation'] = $this->siteConfig->getMenu();
+
+        $this->renderView('register');
+    }
+
+    /**
+     * The post connection method
+     */
+    public function connection()
+    {
+        //is post
+    }
+
+    /**
+     * the post registration method
+     */
+    public function registration()
+    {
+        //is post
+    }
+
+    /*
+     *-----------------------------------------------------------------------------------------
+     *                     Temp connections for testing
+     *-----------------------------------------------------------------------------------------
+     */
+
     //all the connects will finaly be got in a single function grabbed from the DB / Session.
     //this is just for testing purposes until the core framework is finished
     public function connectAdmin()
