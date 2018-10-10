fa4d1afa5d734083b66cc9575abb8689f8c11afc
StarbugStone <pint2stone@gmail.com>
2018-10-10 13:57:48 +0200

    removing the on submit js calls as we do not need them wit the form validation plugin

--- public/js/script.js
+++ public/js/script.js
@@ -51,17 +51,3 @@ if ($(window).width() > MQL) {
 //---------------------------------------------
 /*global WOW*/
 new WOW().init();
-
-
-//---------------------------------------------
-// Login and registration functions
-//---------------------------------------------
-//called on user registration submit
-function userRegistration(){
-
-}
-
-//Called on user Login submit
-function userLogin(){
-
-}
